193d1684a57bad6e0adc119b18a0fbd3d13a12d1
J2FX <j2fxtecnologia@gmail.com>
2025-05-04 18:05:37 -0300

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 (async () => {
   // CONFIGURAÇÃO
-  const supabaseUrl = 'https://cnhqgmfegawkjbiwgvef.supabase.co';
+  const supabaseUrl = 'https://cnhgqmfegawkjbiwgvef.supabase.co';
   const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNuaGdxbWZlZ2F3a2piaXdndmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYyMTA5MDUsImV4cCI6MjA2MTc4NjkwNX0.SjMbOC1zmsorsx8c9658Mu2MZQOpEQtT5jtNcUdAsl4';
   const site = window.location.hostname;
   const page = window.location.pathname === "/" ? "home" : window.location.pathname.replace(/\//g, '');
